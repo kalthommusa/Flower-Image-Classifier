5567be89b6f22a072ae7f2c581b35ab4935ad875
Kalthom Musa <kalthommusa12345@gmail.com>
2022-03-16 02:12:26 +0300

    Update file

--- predict.py
+++ predict.py
@@ -24,11 +24,11 @@ saved_keras_model = args.saved_keras_model
 top_k = args.top_k
 category_names = args.category_names
 
-
-print('Path of the input test image:', image_path) 
-print('Path of the saved keras model:', saved_keras_model)
+  
+print('Path to the input test image:', image_path) 
+print('Path to the saved keras model:', saved_keras_model)
 print('top k class probabilities:',top_k) 
-print('Path of the json file:', category_names)
+print('Path to the json file:', category_names)
 
 
 # Load a JSON file that maps the class values to other category names
@@ -71,9 +71,8 @@ def predict(image_path, model, top_k):
 
 # Call the function that retures the top K classes along with associated probabilities
 top_k_probs, top_k_classes = predict(image_path, loaded_keras_model, top_k) 
+print('List of flower labels along with corresponding probabilities:', top_k_classes, top_k_probs)
 
-
-for i in range(len(top_k_classes)):
-    print('Flower Name:', class_names.get(str(top_k_classes[i]+1)))
-    print('Class Probabilties:', top_k_probs[i])
-
+for flower in range(len(top_k_classes)): 
+    print('Flower Name:', class_names.get(str(top_k_classes[flower]+1)))
+    print('Class Probabilty:', top_k_probs[flower])
